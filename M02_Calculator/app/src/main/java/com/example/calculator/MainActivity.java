d231bb76f7b3a9c1460cada469a7f935827d99d1
BOOG77 <dawsonb14@hotmail.com>
2025-11-09 19:53:38 -0400

    code review updated november 9

--- M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,17 +1,19 @@
+/*
+Student Name: Dawson Brown
+Student Number w0468898
+Due Date: November 9, 2025
+Course: Mobi3002 Mobile Application Development
+Instructor: Russell Shanahan
+Assignment 09
+*/
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.accessibilityservice.AccessibilityGestureEvent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Action when "Add" button is pressed
         ImageButton addButton = (ImageButton) findViewById(R.id.addImageButton);
-        //Button addButton = (Button) findViewById(R.id.b_Add);
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d("M01_Calculator ADD BUTTON", "User tapped the Add button");
@@ -30,19 +31,19 @@ public class MainActivity extends AppCompatActivity {
                 //Log.d("M01_Calculator ADD BUTTON", "button =>"+addButton.getText());
                 Log.d("M01_Calculator ADD BUTTON", "button =>"+addButton.getId());
 
-                Double d1 = 0.0;
-                Double d2 = 0.0;
-                Double answer = 0.0;
+                double firstNumber = 0.0;
+                double secondNumber = 0.0;
+                double answer = 0.0;
 
-                EditText textN1 = (EditText) findViewById(R.id.input1);
-                EditText textN2 = (EditText) findViewById(R.id.input2);
+                EditText textN1 = findViewById(R.id.input1);
+                EditText textN2 = findViewById(R.id.input2);
                 // we actually don't need to get ans from screen
-                EditText textANS = (EditText) findViewById(R.id.answer);
+                EditText textANS = findViewById(R.id.answer);
 
                 try {
-                    d1 = Double.parseDouble(textN1.getText().toString());
-                    d2 = Double.parseDouble(textN2.getText().toString());
-                    answer = d1 + d2;
+                    firstNumber = Double.parseDouble(textN1.getText().toString());
+                    secondNumber = Double.parseDouble(textN2.getText().toString());
+                    answer = firstNumber + secondNumber;
                 }
                 catch (Exception e) {
                     Log.w("M01_Calculator ADD BUTTON", "Add Selected with no inputs ... " + answer);
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 textANS.setText(answer.toString());
 
                 // log what we are doing
-                Log.w("M01_Calculator ADD BUTTON", "Add Selected with => " + d1 + " + " + d2 + "=" + answer);
+                Log.w("M01_Calculator ADD BUTTON", "Add Selected with => " + firstNumber + " + " + secondNumber + "=" + answer);
             }
         });
 
